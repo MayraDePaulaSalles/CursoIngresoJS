cd6570db539ddc358c41deb2489d327350462749
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-08 20:32:27 -0300

    EjerciciosIF

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,20 @@
+/*
+Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive.*/
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	//declaramos variables
+	let numRamdom;
+	let mensaje;
+
+	//Generamos un numero RAMDOM entre el 1 y 10}
+	numRamdom = Math.floor(Math.random() * 10 + 1);
+
+	//concatenamos el mensaje
+	mensaje = "numeroRamdom:" + numRamdom;
+
+    console.log(mensaje);
+
+
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,29 @@
+/*
+Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4.*/
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	//declaramos variables
+	let nota;
+	let mensaje;
+
+	nota = Math.floor(Math.random()* 10 + 1);
+
+	if(nota >= 9){
+		mensaje= "Su nota es:" + nota + "Excelente";
+	}else{
+		if(nota >= 4)
+		{
+			mensaje= "Su nota es:" + nota + "Aprobado";
+		}else
+		{
+			mensaje= "Su nota es:" + nota + "Desaprobado";
+		}
+
+		}
+		alert(mensaje);
+	
 
 }//FIN DE LA FUNCIÓN
