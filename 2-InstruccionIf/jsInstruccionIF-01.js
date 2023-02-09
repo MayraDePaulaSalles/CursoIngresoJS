df6206306e87ef5ba42de141c73ece811b9881b2
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-09 19:10:04 -0300

    ejercicioIF. BisBis

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -31,7 +31,7 @@ el tiempo que tardo, para calcular la velocidad e informar:
 hasta 80km/h= lento 
 hasta 100= buen ritmo
 hasta 120= ahi de la ley 
-mas = eso no se hace*/
+mas = eso no se hace
 function mostrar()
 {
 	//declaramos las variables
@@ -70,6 +70,80 @@ function mostrar()
 	}
 
 	 alert(mensaje);
+
+
+}*/
+
+/*
+Mayra De Paula Salles - Ejercicio 1.bis bis IF
+con if ingresar el nombre y los datos requeridos para calcular el IMC , e informar a la persona si es:
+Bajo peso <18.5
+Peso normal 18,5-24.9
+Preobesidad 25-26.9
+Obesidad I 27-29.9
+Obesidad II 30-34.9
+Obesidad III >40.*/
+function mostrar()
+{
+	//DECLARAMOS VARIABLES
+	let nombre;
+	let IMC;
+	let peso;
+	let altura;
+	let mensaje;
+
+	//ASIGNAMOS VALOR POR ID
+	nombre = document.getElementById("txtIdEdad")
+
+	//INGRESAMOS POR PROMPT
+	peso = prompt("Cuanto pesas?");
+	altura = prompt("Cuanto medis?");
+	
+	//PARSEAMOS
+	peso = parseInt(peso);
+	altura = parseFloat(altura);
+
+	//SACAMOS EL IMC
+	IMC = peso / (altura * altura);
+
+	if(IMC < 18.5){
+		mensaje = IMC + "Bajo peso";
+	}else{
+		
+		if(IMC > 18.5 && IMC <= 24.9){
+			mensaje = IMC + "Peso normal";
+		}else{
+
+			if(IMC > 24.9 && IMC <= 26.9){
+				mensaje = IMC + "Preobesidad";
+			}else{
+
+				if(IMC > 26.9 && IMC <= 29.9){
+					mensaje = IMC + "Obesidad I";
+				}else{
+
+					if(IMC > 29 && IMC <= 34.9){
+						mensaje = IMC + "Obesidad II";
+					}else{
+
+						if(IMC > 35){
+							mensaje = IMC + "Obesidad III";
+						}
+					}
+				}
+			}
+			
+		}
+		
+	}
+    
+	alert(mensaje);
+
+
+
+
+
+
 
 
 }
