db4f1b93bfe4fe86830630a82ed9e23d15c63942
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-09 17:19:54 -0300

    EjercicioIF.bis

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,5 +1,5 @@
 /*
-Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita".*/
+Al ingresar una edad que sea igual a 15, mostrar el mensaje "niña bonita".
 
 function mostrar()
 {
@@ -21,4 +21,56 @@ function mostrar()
 	}
 	
 	//FIN DE LA FUNCIÓN
+}*/
+
+/*
+Mayra De Paula Salles - Ejercicio 1.bis IF
+con if pedir a una persona que fue de vacaciones , la distancia que viajo y 
+el tiempo que tardo, para calcular la velocidad e informar:
+60 km/hr = muy lento 
+hasta 80km/h= lento 
+hasta 100= buen ritmo
+hasta 120= ahi de la ley 
+mas = eso no se hace*/
+function mostrar()
+{
+	//declaramos las variables
+	let distanciaDeViaje;
+	let tiempoDeViaje;
+	let mensaje;
+
+	distanciaDeViaje = document.getElementById("txtIdEdad").value;
+	tiempoDeViaje = prompt("Cuantos km/hr recorrio?");
+	
+	//parseamos
+	distanciaDeViaje = parseInt(distanciaDeViaje);
+	tiempoDeViaje = parseInt(tiempoDeViaje);
+
+	
+   if(distanciaDeViaje && tiempoDeViaje == 60){
+		mensaje= "Muy lento";
+	}else{
+		
+		if(distanciaDeViaje && tiempoDeViaje == 80){
+			mensaje= "Lento";
+		}else{
+			
+			if(distanciaDeViaje && tiempoDeViaje == 100){
+				mensaje= "Buen ritmo";
+			}else{
+
+				if(distanciaDeViaje && tiempoDeViaje == 120){
+					mensaje= "Ahi de la ley";
+				}else{
+                     mensaje= "Eso no se hace";
+					
+				}
+			}
+		}
+	}
+
+	 alert(mensaje);
+
+
 }
+
