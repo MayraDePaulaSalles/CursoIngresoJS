101f63869d73fc823f771023a46641c463c12a66
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-11 20:30:50 -0300

    TP4 - SWITCH/IF

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -7,7 +7,7 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
- */
+ 
 function CalcularPrecio () 
 {
     //declaramos las variables
@@ -79,4 +79,155 @@ function CalcularPrecio ()
     document.getElementById("txtIdprecioDescuento").value = mensaje;
 
 
+}*/
+
+/* TP4 solo SWITCH (las marcas) y con IF (cantidad)*/
+function CalcularPrecio ()
+{
+    //declaramos las variables
+    let cantLamparas;
+    let marca;
+    let descuento = 0;
+    let aumento = 10;
+    let precioInicial;
+    let precioDescuento;
+    let precioFinal;
+    let impuesto;
+    let precioImpuesto;
+    let mensaje;
+    const PRECIO = 35;
+
+    //ingresamos valores por Id
+    cantLamparas = document.getElementById("txtIdCantidad").value;
+    marca = document.getElementById("Marca").value;
+    //parseamos
+    cantLamparas = parseInt(cantLamparas);
+
+    //calculamos el precio inicial
+    precioInicial = cantLamparas * PRECIO;
+
+    switch(marca)
+    {
+        case "ArgentinaLuz":
+            if(cantLamparas >= 6){
+                descuento = 50;
+            }else{
+                if(cantLamparas == 5){
+                    descuento = 40;
+                }else{
+                   if(cantLamparas == 4){
+                        descuento = 25;
+                    }else{
+                       if(cantLamparas == 3){
+                            descuento = 15;
+                        }else{
+                            descuento = 0;
+                        }
+                    }
+                }
+            }
+            break;
+        case "FelipeLamparas":
+            if(cantLamparas >= 6){
+                descuento = 50;
+            }else{
+                if(cantLamparas == 5){
+                    descuento = 30;
+                }else{
+                    if(cantLamparas == 4){
+                        descuento = 25;
+                    }else{
+                        if(cantLamparas == 3){
+                            descuento = 10;
+                        }else{
+                            descuento = 0;
+                        }
+                    }
+                }
+            }
+            break;
+        case "JeLuz":
+            if(cantLamparas >5){
+                descuento = 50;
+            }else{
+                if(cantLamparas == 5){
+                    descuento = 30;
+                }else{
+                    if(cantLamparas == 4){
+                        descuento = 20;
+                    }else{
+                        if(cantLamparas == 3){
+                            descuento = 5;
+                        }else{
+                            descuento = 0;
+                        }
+
+                        
+                    }
+                }
+            }
+            break; 
+        case "Haslluminacion":
+            if(cantLamparas >5){
+                descuento = 50;
+            }else{
+                if(cantLamparas == 5){
+                    descuento = 30;
+                }else{
+                    if(cantLamparas == 4){
+                        descuento = 20;
+                    }else{
+                        if(cantLamparas == 3){
+                            descuento = 5;
+                        }else{
+                            descuento = 0;
+                        }
+                    }
+                }
+            }
+            break;
+        case "Osram":
+            if(cantLamparas > 5){
+                descuento = 50;
+            }else{
+                if(cantLamparas == 5){
+                    descuento = 30;
+                }else{
+                    if(cantLamparas == 4){
+                        descuento = 20;
+                    }else{
+                        if(cantLamparas == 3){
+                            descuento = 5;
+                        }else{
+                            descuento = 0
+                        }
+                    }
+                }
+            }
+            break;
+        
+    }
+
+    //calculamos el precio final con descuento
+    precioDescuento = precioInicial * descuento / 100;
+    precioFinal = precioInicial - descuento;
+
+    //concatenamos el mensaje para el precio final con descuento
+    mensaje = "Precio Final:" + "$" + precioFinal;
+
+    //condicion si el precio final supera los $120
+    if(precioFinal >= 120){
+        impuesto = precioFinal * aumento / 100;
+        precioImpuesto = precioFinal + impuesto;
+        alert("Usted pago $" + precioImpuesto + "de IIBB., siendo $" + impuesto + "el impuesto que se pagó")
+    }
+
+    //muestro los resultados con descuento
+    document.getElementById("txtIdprecioDescuento").value = mensaje;
+
+
+
 }
+
+
+
