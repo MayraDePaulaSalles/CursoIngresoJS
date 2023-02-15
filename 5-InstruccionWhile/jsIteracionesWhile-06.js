f1daee0cb7c5e8385a7f882993af4c1324e9fb1b
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-15 10:28:06 -0300

    Ejercicio 6 WHILE/FOR

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,79 @@
+/*
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	//declaramos variables
+	let numero;
+	let contador;
+	let acumulador;
+	let promedio;
+	let i;
+    
+	contador = 0;
+	acumulador = 0;
+    i = 0;
 
-	contador=0;
-	acumulador=0;
+	//parseamos e ingresamos por prompt
+	numero = parseInt(prompt("Ingresar un numero:"));
 	
+	while(i < 4)
+	{
+		numero = parseInt(prompt("Ingresar un numero:"));
+		while(isNaN(numero)) //validacion
+		{
+			numero = prompt("Error, ingrese un numero");
+			numero = parseInt(numero);
+		}
+		
+		contador = contador + 1;
+		acumulador = acumulador + numero;
+
+		i = i + 1;
+	}
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	//sacamos el promedio
+	promedio = acumulador / contador;
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
+
+	
+}//FIN DE LA FUNCIÓN*/
+
+/*Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.*/
+function mostrar()
+{
+	//declaro variables
+	let numeroIngresado;
+	let contador = 0;
+	let acumulador = 0;
+	let promedio;
+
+	for(let i = 0;              i < 5;            i++)
+	{
+		numero = parseInt(prompt("Ingresar un numero:"));
+		while(isNaN(numero)) //validacion
+		{
+			numero = prompt("Error, ingrese un numero");
+			numero = parseInt(numero);
+		}
+		
+		contador = contador + 1;
+		acumulador = acumulador + numero;
+
+	}
+
+	//sacamos el promedio
+	promedio = acumulador / contador;
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
+
+	
+
+
+
+
+
+
+}
