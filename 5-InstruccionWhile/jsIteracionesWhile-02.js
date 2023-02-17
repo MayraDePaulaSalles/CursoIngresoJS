c5792a8d29e6bfd6b4ba8f9f94236b3b025eda17
MayraDePaulaSalles <depaulasallesm@gmail.com>
2023-02-17 01:03:56 -0300

    ejBisBisWhile2

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,6 +1,6 @@
 /*
 al presionar el botón mostrar 10 repeticiones 
-con números DESCENDENTES, desde el 10 al 1.*/
+con números DESCENDENTES, desde el 10 al 1.
 function mostrar()
 {
 	//declaro  una variable
@@ -20,4 +20,58 @@ function mostrar()
 	
 	alert('iteración while');
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN*/
+
+/*
+while 2 bis: Ingresar 5 números, y determinar la cantidad de números que son mayores 
+que 10 y menores 20 (inclusive en ambos casos)*/
+function mostrar()
+{
+	//declaramos las variables
+	let numeroIngresado;
+	let i;
+	let contadorMayor;
+	let contadorMenor;
+	let mensaje;
+
+	//inicializo las variables
+	i = 0;
+	contadorMayor = 0;
+	contadorMenor = 0;
+
+	while(i < 5)
+	{
+		numeroIngresado = parseInt(prompt("Ingrese un numero"));
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado = parseInt(prompt("Error, ingrese un numero"));
+		}
+		if(numeroIngresado > 10)
+		{
+			contadorMayor = contadorMayor + 1;
+		}
+		else if(numeroIngresado < 20)
+		{
+			contadorMenor = contadorMenor + 1;
+		}
+		
+		i = i + 1;
+	}   
+	mensaje = "Los numeros mayores que 10 son" + contadorMayor + "y los numeros menores que 20 son" + contadorMenor;
+	alert(mensaje);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
